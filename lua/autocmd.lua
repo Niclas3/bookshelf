ae5fecbd148fad82c7ce52459a06acde88855661
Min <missingurchin@gmail.com>
2022-11-17 18:00:36 +0800

    some README

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -51,6 +51,18 @@ if has("autocmd")
 
     " Group end
     augroup END
+
+    " for hex editing
+    augroup Binary
+        au!
+        au BufReadPre  *.bin let &bin=1
+        au BufReadPost *.bin if &bin | %!xxd
+        au BufReadPost *.bin set ft=xxd | endif
+        au BufWritePre *.bin if &bin | %!xxd -r
+        au BufWritePre *.bin endif
+        au BufWritePost *.bin if &bin | %!xxd
+        au BufWritePost *.bin set nomod | endif
+    augroup END
 endif
 ]])
 end
